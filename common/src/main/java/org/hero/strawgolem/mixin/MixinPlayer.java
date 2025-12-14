9af4934d9fb2a231445fdeea8713b30558469673
Hero <herostillplays@gmail.com>
2025-12-14 14:30:16 -0500

    Fixing player mixin crash, for real this time. Thank you @keusta for the original fix attempt, however, it broke golem ordering on servers.

--- common/src/main/java/org/hero/strawgolem/mixin/MixinPlayer.java
+++ common/src/main/java/org/hero/strawgolem/mixin/MixinPlayer.java
@@ -1,25 +1,15 @@
 package org.hero.strawgolem.mixin;
 
 import net.minecraft.world.entity.player.Player;
-import org.hero.strawgolem.Constants;
-import net.minecraft.client.Minecraft;
 import org.hero.strawgolem.golem.StrawGolem;
 import org.hero.strawgolem.mixinInterfaces.GolemOrderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
 public class MixinPlayer implements GolemOrderer {
-    @Inject(at = @At("TAIL"), method = "<init>")
-    private void init(CallbackInfo info) {
-        Constants.LOG.info("This line is printed by the Straw Golem common mixin!");
-        Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
-    }
     @Unique // Optional, but good practice to ensure the name is unique and won't conflict
-    private StrawGolem strawgolemRewrite$strawGolem; // The new field being added
+    private StrawGolem strawgolemRewrite$strawGolem = null; // The new field being added
 
     @Override
     public StrawGolem strawgolemRewrite$getGolem() {
